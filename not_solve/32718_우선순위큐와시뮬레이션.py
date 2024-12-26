71357b7ae41e75cff3225b7af974b4f29de6ddf3
SierraJY <wndus51445@gmail.com>
2024-12-26 14:39:18 +0900

    -

---
+++ not_solve/32718_우선순위큐와시뮬레이션.py
@@ -0,0 +1,29 @@
+import sys
+from bisect import bisect_right
+
+sys.stdin = open('input.txt', 'r')
+N, K, T = map(int, sys.stdin.readline().split())
+Q = list(map(lambda x: int(x), sys.stdin.readline().split()))
+A = list(map(int, sys.stdin.readline().split()))
+
+Q = [q % K for q in Q]  # 모든 원소를 K로 나눈 나머지로 변환
+Q.sort()  # 정렬
+current_add = 0  # 누적 합
+
+for a in A:
+    current_add = (current_add + a) % K  # 누적 합 갱신
+
+    idx = bisect_right(Q, K - 1 - current_add)
+    if idx == 0:  # 모든 값이 범위를 벗어나는 경우
+        max_val = (Q[-1] + current_add) % K
+    else:
+        max_val = max((Q[idx - 1] + current_add) % K, (Q[-1] + current_add) % K)
+
+    print(max_val, end = " ")
+
+# 주의할 것
+# 우선순위 큐 문제라도, 반드시 Heap을 사용하는 문제가 아닐 수 있음
+# 우선순위 큐 != Heap
+
+# 알게된 것
+# 모듈로 연산의 연산 법칙(결합 , 분배)
